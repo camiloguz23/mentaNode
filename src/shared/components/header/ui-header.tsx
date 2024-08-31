37f74962c0f3fc37223bedba58e84b02e3dcbcdf
Camilo Guzman <jeysonkmguzman@gmail.com>
2024-08-30 22:04:17 -0500

    feat: add settings

---
+++ src/shared/components/header/ui-header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./header.module.css";
+
+export function UiHeader() {
+  return (
+    <header className={styles.header}>
+      <h1>Book Note </h1>
+    </header>
+  );
+}
